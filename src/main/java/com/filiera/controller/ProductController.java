ff5ddd87a5e416d67fe6c68657288406bba16214
lollo <lorenzogio.dimichele@studenti.unicam.it>
2025-05-31 12:56:01 +0200

    scheletro

--- src/main/java/com/filiera/controller/ProductController.java
+++ src/main/java/com/filiera/controller/ProductController.java
@@ -1,6 +1,6 @@
 package com.filiera.controller;
 
-import com.filiera.model.Prodotto;
+import com.filiera.model.Products.Prodotto;
 import com.filiera.services.ProductService;
 
 import java.math.BigDecimal;
