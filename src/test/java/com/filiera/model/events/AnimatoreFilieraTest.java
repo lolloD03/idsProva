0fa3541b7eab2d9d28de75fd0e9509ec95cddaff
Maffy03 <davide.mafalda@studenti.unicam.it>
2025-06-04 16:30:08 +0200

    Prova

---
+++ src/test/java/com/filiera/model/events/AnimatoreFilieraTest.java
@@ -0,0 +1,32 @@
+package com.filiera.model.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimatoreFilieraTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEventsCreated() {
+    }
+
+    @Test
+    void setEventsCreated() {
+    }
+}
