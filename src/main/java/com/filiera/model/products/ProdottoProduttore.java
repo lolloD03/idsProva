b112b2d116209ce7ca8c5b7ed4743ea8b46b7ab2
Maffy03 <davide.mafalda@studenti.unicam.it>
2025-06-05 17:38:18 +0200

    Prova

--- src/main/java/com/filiera/model/products/ProdottoProduttore.java
+++ src/main/java/com/filiera/model/products/ProdottoProduttore.java
@@ -1,6 +1,28 @@
 package com.filiera.model.products;
 
+import com.filiera.model.sellers.Venditore;
+
 public class ProdottoProduttore extends Prodotto {
+
+
+    private String cultivationProcess;
+
+    public ProdottoProduttore() {
+        super();
+    }
+
+    public ProdottoProduttore(String name, String description, double price, int quantity, Venditore seller, int daysToExpire, String cultivationProcess) {
+        super(name, description, price, quantity, seller, daysToExpire);
+        this.cultivationProcess = cultivationProcess;
+    }
+
+    public String getCultivationProcess() {
+        return cultivationProcess;
+    }
+
+    public void setCultivationProcess(String cultivationProcess) {
+        this.cultivationProcess = cultivationProcess;
+    }
 
 
 
